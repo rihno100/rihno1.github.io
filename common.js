914cd148cf624d91a490f09201a5fa02212b3b3a
rihno1 <wizard99v@naver.com>
2018-06-17 18:38:36 +0900

    index_update

--- common.js
+++ common.js
@@ -17,4 +17,21 @@ $(document).ready(function(){
     $(this).css("display","none")
     $(".avaliabletool,.profilearea").css("display","none")
   });
+
+  $("#navmap p").on({
+    click: function(){
+      const thisIndex = $(this).index();
+      console.log(thisIndex)
+      const onLeft = $(this).css("left");
+      const onTop = $(this).css("top");
+      /////초기화/////
+      $("body").removeClass();
+      /////실행/////
+      $("#navmap .on").css({"left":onLeft,"top":onTop});
+      if(thisIndex != 0 ){
+        $("body").addClass("sectionarea"+thisIndex);
+      }
+    }
+  })
+
 });
