7a31f0ecb738d7e1c65f56a44d2e944bb33d6ac6
rihno1 <wizard99v@naver.com>
2018-06-20 02:38:07 +0900

    ordinary_section_update

--- common.js
+++ common.js
@@ -32,9 +32,11 @@ $(document).ready(function(){
   });
 
 
+
+/******경력섹션******/
   const ctaWidth = $(".careertreinarea > .items").length * $(".careertreinarea > .items").width();
   const ctaItemWidth = $(".careertreinarea > .items").width();
-  const docWidth = $(document).width();
+  var docWidth = $(document).width();
 
 
 
@@ -78,7 +80,7 @@ $(document).ready(function(){
     }
   })
 
-
+/********사이드 메뉴 맵메뉴 메칭**********/
   $("aside ul li").each(function(i){
     if(i == 1){
       $(this).find("a").on({
@@ -112,6 +114,93 @@ $(document).ready(function(){
     click : function(){
       $("#navmap p:nth-of-type(1)").trigger("click");
     }
-  })
+  });
+
+
+/*******my ordinnary *******/
+  /******클릭이후 엑션******/
+  var gItemLeft,gItemTop,gItemSize,gelem;
+    $(".galleryitems").on({
+      click : function(e){
+
+        if($(".galleryitems").hasClass("over") != true){// default 상태 클릭
+          gItemLeft = $(this).css("left");
+          gItemTop = $(this).css("top");
+          gItemSize = $(this).css("width");
+          gelem  = $(this);
+
+          gelem.addClass("over");
+          gelem .css({"left":"50%","top":"50%","width": "500px","height": "500px"})
+
+        }else{
+          gelem.removeClass("over");
+          gelem.css({"left":gItemLeft,"top":gItemTop,"width": gItemSize,"height":gItemSize})
+        }
+      }
+    });
+
+
+//var data = $(this).dataTransfer.setData("moveposition",$(this))
+  //const container = document.getElementsByClassName('section-ctn')[0];
+  var dragArea = document.body;
+  var dragItemLength = document.getElementsByClassName('galleryitems').length;
+  var dragItems = document.querySelectorAll('.galleryitems');
+
+  for(var i=0 ; i < dragItemLength ; i++ ){
+      dragItems[i].addEventListener("dragstart", dragstart);
+      dragArea.addEventListener("dragover", dragover);
+      dragArea.addEventListener("drop", drop);
+  }
+
+
+  function dragstart(e) {
+    e.dataTransfer.setData("moveelem",e.target.parentNode.id);
+    //console.log(e.target.parentNode.id)
+  }
+  function dragleave(e) {
+    e.preventDefault()
+    console.log("dragleave")
+  }
+  function dragover(e) {
+    e.preventDefault()
+    console.log("dragover")
+  }
+  function dragenter(e) {
+    e.preventDefault()
+    console.log("dragenter")
+  }
+  function drop(e) {
+    //this.append(box)
+    var data = e.dataTransfer.getData("moveelem");
+    $("#"+data).css({"left":e.pageX+"px","top":e.pageY+"px"});
+    console.log("drop"+data)
+  }
+
+  /********sorting my ordinnary thumbnail  when window resize**********/
+  $(window).resize(function(){
+    var wdWidth = $(this).width();
+    var wdHeight = $(this).height();
+
+    $(".galleryitems").each(function(){
+      
+      var elemSize = Math.random() * 400;
+      while(elemSize < 50){
+        elemSize = Math.random() * 400
+      }
+
+      var elemLeft = Math.random() * (wdWidth - 400);
+      while(elemLeft < 50){
+        elemLeft = Math.random() * (wdWidth - 400);
+      }
+
+      var elemTop = Math.random() * (wdHeight - 400);
+      while(elemTop < 100){
+        elemTop = Math.random() * (wdHeight - 400);
+      }
+
+      $(this).css({"left": elemLeft+"px","top": elemTop+"px","width": elemSize+"px","height": elemSize+"px"});
+    });
+  });
+
 
 });
